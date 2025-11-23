2cd804f54285d3f0318c41bcbc9f5fac8cd29eb3
Mustafa <mustafa.awami1@gmail.com>
2025-11-23 20:38:15 +0300

    Add workbench with User model and factory

--- database/migrations/2025_09_10_081543_create_passkeys_table.php
+++ database/migrations/2025_09_10_081543_create_passkeys_table.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,7 +14,7 @@ return new class extends Migration
         Schema::create('passkeys', function (Blueprint $table) {
             $table->id();
 
-            $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
 
             $table->string('name');
             $table->string('credential_id');
